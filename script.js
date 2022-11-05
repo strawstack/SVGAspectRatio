46c7ec0cf482321d61f108d1bed26346bf13b641
Richard <rhshadowgreen@gmail.com>
2022-11-04 19:57:19 -0700

    Update script.js

--- script.js
+++ script.js
@@ -4,25 +4,31 @@ function main() {
         .append("circle")
             .attr("cx", "0")
             .attr("cy", "0")
-            .attr("r", "25");
+            .attr("r", "40");
 
     d3.select("svg")
         .append("circle")
             .attr("cx", "1280")
             .attr("cy", "0")
-            .attr("r", "25");
+            .attr("r", "40");
 
     d3.select("svg")
         .append("circle")
             .attr("cx", "0")
             .attr("cy", "1280")
-            .attr("r", "25");
+            .attr("r", "40");
 
     d3.select("svg")
         .append("circle")
             .attr("cx", "1280")
             .attr("cy", "1280")
-            .attr("r", "25");
+            .attr("r", "40");
+    
+    d3.select("svg")
+        .append("circle")
+            .attr("cx", "640")
+            .attr("cy", "640")
+            .attr("r", "40");
 }
 
 window.onload = main;
