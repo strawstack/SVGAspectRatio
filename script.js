f39843a67b8ed50ce4387ac9c60dea999cf1014c
Richard <rhshadowgreen@gmail.com>
2022-11-06 13:31:19 -0800

    Update script.js

--- script.js
+++ script.js
@@ -17,19 +17,19 @@ function main() {
     d3.select("svg")
         .append("circle")
             .attr("cx", "0")
-            .attr("cy", "1280")
+            .attr("cy", "720")
             .attr("r", r);
 
     d3.select("svg")
         .append("circle")
             .attr("cx", "1280")
-            .attr("cy", "1280")
+            .attr("cy", "720")
             .attr("r", r);
     
     d3.select("svg")
         .append("circle")
             .attr("cx", "640")
-            .attr("cy", "640")
+            .attr("cy", "360")
             .attr("r", r);
 }
 
